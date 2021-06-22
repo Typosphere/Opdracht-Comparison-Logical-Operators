c7ca89f1a3309bf8556402ee59af2324974e8b94
Typosphere <67193118+Typosphere@users.noreply.github.com>
2021-06-22 12:24:50 +0200

    Add files via upload

---
+++ Opdracht-Comparison Logiacal Operators/index.js
@@ -0,0 +1,67 @@
+// const age = 14; //expression
+
+// if (age >= 18){
+//     console.log ("Welcome");
+
+// } else if (age < 18){
+//     console.log ("Sorry, we ask you to go outside, thank you.");
+// } else
+
+// const isFemale = true;
+
+// if (isFemale){
+//     console.log ("Welcome, it's ladynight");
+// } else (isFemale)
+//     console.log ("Sorry, only ladies tonight");
+
+
+// const driverStatus = "BoB";
+
+// if (driverStatus){
+//     console.log ("You are BoB");
+// } else 
+//     console.log ("You are not BoB, you can't drive");
+
+// Deel 1 50% korting
+
+const age = 30;
+
+if (age >= 18 && age <= 25){
+    console.log("Je krijgt 50% korting!")
+
+}else (age)
+    console.log("Helaas, geen korting.");
+
+
+    // DEEL 2: Ludieke actie!
+
+const name = "Bram";
+
+
+if (name === "Bram" || name === "Sarah"){
+    console.log("Je krijgt een gratis biertje")
+
+}else (name)
+    console.log("Jammer, voor jou geen biertje!");
+
+
+    // DEEL 3: Jubileum korting
+
+const totalAmount = "Jubileum korting";
+
+if (totalAmount >= 25){
+    console.log("Yeah, gratis bitterballen")
+
+} else if (totalAmount >= 50)
+    console.log("Yeah, je krijgt gratis portie nachos")
+
+
+else (totalAmount >= 100)
+    console.log("Yeah, voor jou een fles champagne");
+
+
+
+
+
+
+
